8cc0f95383bbd2ee679fce93a265e2631fcd08eb
traineedevops <a_zayats@outlook.com>
2019-03-14 11:23:37 +0200

    [ADD] Added USER vars to deployment script

--- add_tag_to_EC2/deploy_lambda_with_SAM.sh
+++ add_tag_to_EC2/deploy_lambda_with_SAM.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
+
+AWS_USER=mycli
+
 ##################################################################
 #
 #		Deploy Lambda function using SAM via AWS CLI 
 #
 ##################################################################
 
-aws s3 mb s3://ec2-owner-tag-$USERNAME	 # Create S3 bucket to store your deployment packeges
+aws s3 mb s3://ec2-owner-tag-$AWS_USER	 # Create S3 bucket to store your deployment packeges
 
 
-aws cloudformation package --template-file template.yaml --s3-bucket ec2-owner-tag-$USERNAME --output-template-file output.yaml # Create deployment package && upload to S3.
+aws cloudformation package --template-file template.yaml --s3-bucket ec2-owner-tag-$AWS_USER --output-template-file output.yaml # Create deployment package && upload to S3.
 
 
 aws cloudformation deploy --stack-name ec2-owner-tag --template-file output.yaml --capabilities CAPABILITY_IAM # Deploy lambda
@@ -17,4 +21,14 @@ aws cloudformation deploy --stack-name ec2-owner-tag --template-file output.yaml
 # P.S. 
 # 
 #	Make sure your working directory containe template YAML's...
+#
+##################################################################
+#	DELETE stack :
+#
+#		aws cloudformation delete-stack --stack-name ec2-owner-tag
+#	
+#	DELETE S3 Bucket :
+#	
+#		aws s3 rb s3://ec2-owner-tag-$AWS_USER --force
+#
 ##################################################################
